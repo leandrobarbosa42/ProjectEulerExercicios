fd9305c294708a8ab8995653dbd49ec3df0bd326
Leandro Barbosa <84480197+leandrobarbosa42@users.noreply.github.com>
2025-07-01 21:10:04 -0300

    Update ehPrimo.py

--- python/ehPrimo.py
+++ python/ehPrimo.py
@@ -1,7 +1,4 @@
-#Codigo utilizando a tecnica de "Testemunha de primalidade"
-#Essa técnica se baseia no fato de que, se um número não é primo, 
-#ele pode ser escrito como um produto de dois fatores, 
-#sendo que pelo menos um deles é menor ou igual à sua raiz quadrada.
+
 
 def ehPrimo(n):
     if n < 2 : return False
